128c9f9cc3983e635f1e7fb0ef1b1d2380e1b871
mitschix <23629789+mitschix@users.noreply.github.com>
2020-03-11 23:06:06 +0100

    autodetect notify program

--- config/py3status/todo.sh
+++ config/py3status/todo.sh
@@ -2,7 +2,12 @@
 
 echo " $(calcurse -t | tail -n +2 | wc -l)"
 
+if [ $(command -v dunstify) ]
+    then notifiy="dunstify -r 4"
+    else notifiy="notify-send"
+fi
+
 case $BLOCK_BUTTON in
-  1) dunstify -r 4 -i view-list -a status "TODOs" "$(calcurse -t | tail -n +2 )" ;;
+  1) $notifiy -i view-list -a status "TODOs" "$(calcurse -t | tail -n +2 )" ;;
 	2) $TERMINAL -e calcurse ;;
 esac
--- config/py3status/weather.sh
+++ config/py3status/weather.sh
@@ -5,11 +5,16 @@ getforecast() { ping -q -c 1 1.1.1.1 > /dev/null 2>&1 || echo ""
 curl -s "wttr.in/$location" > "$HOME/.local/share/weatherreport" 2> /dev/null || echo "";}
 
 
+if [ $(command -v dunstify) ]
+    then notifiy="dunstify -r 4"
+    else notifiy="notify-send"
+fi
+
 showweather() { printf "%s" "$(sed '16q;d' "$HOME/.local/share/weatherreport" | grep -wo "[0-9]*%" | sort -n | sed -e '$!d' | sed -e "s/^/☔ /g" | tr -d '\n')"
 sed '13q;d' "$HOME/.local/share/weatherreport" | grep -o "m\\(-\\)*[0-9]\\+" | sort -n -t 'm' -k 2n | sed -e 1b -e '$!d' | tr '\n|m' ' ' | awk '{print " ❄️",$1 "°","",$2 "°"}' ;}
 
 case $BLOCK_BUTTON in
-    1) $TERMINAL -e less -RS "$HOME/.local/share/weatherreport" ;;
+    # 1) $TERMINAL -e less -RS "$HOME/.local/share/weatherreport" ;;
     2) getforecast && showweather ;;
     # need libnotify-bin installed
 #    3) pgrep -x dunst >/dev/null && notify-send "🌈 Weather module" "\- Left click for full forecast.
