eb2a71d9aafe7c5e7b417299b184bf6f52a11367
mitschix <23629789+mitschix@users.noreply.github.com>
2020-06-09 23:56:03 +0200

    add automatic extension install script

---
+++ config/vscodium/install_extensions.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+vscodium \
+    --install-extension almenon.arepl \
+    --install-extension ban.spellright \
+    --install-extension Gruntfuggly.todo-tree \
+    --install-extension James-Yu.latex-workshop \
+    --install-extension kiteco.kite \
+    --install-extension ms-python.python \
+    --install-extension ms-vsliveshare.vsliveshare \
+    --install-extension streetsidesoftware.code-spell-checker \
+    --install-extension streetsidesoftware.code-spell-checker-german \
+    --install-extension vscode-snippet.snippet \
+    --install-extension vscodevim.vim \
+    --install-extension yzane.markdown-pdf \
+    --install-extension yzhang.markdown-all-in-one \
+    --install-extension atishay-jain.all-autocomplete\
