fbf057edd3dc14eba145cc20996e9bb312b2227a
mitschix <23629789+mitschix@users.noreply.github.com>
2020-09-12 00:28:40 +0200

    format file with pep8

--- bin/f_renamer.py
+++ bin/f_renamer.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python 
-# Pythono3 code to rename multiple  
-# files in a directory or folder 
-import os 
+#!/usr/bin/env python
+# Pythono3 code to rename multiple
+# files in a directory or folder
+import os
 
-# Function to rename multiple files 
+# Function to rename multiple files
+
+
 def f_renamer(directory, string=None):
     dir_list = os.listdir(directory)
     dir_list.sort()
-    for count, filename in enumerate(dir_list): 
+    for count, filename in enumerate(dir_list):
         ext = filename.split(".")[-1]
         dstname = f"{count:04d}_{string}.{ext}"
         src = os.path.join(directory, filename)
@@ -18,15 +20,16 @@ def f_renamer(directory, string=None):
         else:
             print(f"Change {filename} to {dstname}.")
 
-        # rename() function will 
-        # rename all the files 
-        os.rename(src, dst) 
+        # rename() function will
+        # rename all the files
+        os.rename(src, dst)
 
-def main(): 
+
+def main():
     cwd = os.getcwd()
     string = "wallpaper"
     f_renamer(cwd, string)
-  
-if __name__ == '__main__': 
-    main() 
 
+
+if __name__ == '__main__':
+    main()
