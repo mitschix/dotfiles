5c6dbb22727b69111634d1e92116023b1c5c738b
mitschix <23629789+mitschix@users.noreply.github.com>
2021-08-22 21:54:13 +0200

    add WTC to i3status fortune; fix ssh command for alacritty; update nvim sub

--- config/i3/fortune_bar.sh
+++ config/i3/fortune_bar.sh
@@ -3,6 +3,12 @@ LEN_MAX=90
 BLANKS=$( for _ in range{1..200};do echo -n " ";done )
 
 while true; do
-  echo $(fortune -s -n "$LEN_MAX") "$BLANKS"
-	sleep 30
+    RAND=$((1+$RANDOM % 10))
+    if [ $(($RAND % 2)) == 0 ]
+    then
+        echo "FT - " $(fortune -s -n "$LEN_MAX") "$BLANKS"
+    else
+        echo "WTC - " $(curl -s http://whatthecommit.com/index.txt) "$BLANKS"
+    fi
+    sleep 30
 done
