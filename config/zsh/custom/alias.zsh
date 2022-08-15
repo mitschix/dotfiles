e8ed3d78ca25902ecda2b699324b556dd68b4361
mitschix <23629789+mitschix@users.noreply.github.com>
2022-08-16 00:41:06 +0200

    add new alias

--- config/zsh/custom/alias.zsh
+++ config/zsh/custom/alias.zsh
@@ -117,4 +117,6 @@ alias capmap="setxkbmap -option caps:escape"
 # fi
 # unset is_advcp
 
-alias info_termite='echo "curl https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info | sudo tic -x -" | xsel -bi'
+alias info_term='echo "curl https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info | sudo tic -x -" | xsel -bi'
+alias sort-by-ip="sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4"
+alias remove-color="sed 's/\x1b\[[0-9;]\{1,\}[a-za-z]//g'"
