9e4a5ef62378e2eb6c41c4ec420b9f8d3964b60b
mitschix <23629789+mitschix@users.noreply.github.com>
2025-03-03 23:52:58 +0100

    format code; add https and hint for local server

--- config/i3/fortune_bar.sh
+++ config/i3/fortune_bar.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 LEN_MAX=90
-BLANKS=$( for _ in range{1..200};do echo -n " ";done )
+BLANKS=$(for _ in range{1..200}; do echo -n " "; done)
 
 while true; do
-    RAND=$((1+$RANDOM % 10))
-    if [ $(($RAND % 2)) == 0 ]
-    then
+    RAND=$((1 + $RANDOM % 10))
+    if [ $(($RAND % 2)) == 0 ]; then
         echo "FT - " $(fortune -s -n "$LEN_MAX") "$BLANKS"
     else
-        echo "WTC - " $(curl -s http://whatthecommit.com/index.txt) "$BLANKS"
+        # or use local docker file https://github.com/ngerakines/commitment
+        # echo "WTC - " $(curl -s http://localhost:<port>/index.txt) "$BLANKS"
+        echo "WTC - " $(curl -s https://whatthecommit.com/index.txt) "$BLANKS"
     fi
     sleep 30
 done
