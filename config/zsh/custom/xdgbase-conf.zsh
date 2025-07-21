79226dc83cb049a42c1c7d7c8bf776527527e567
mitschix <23629789+mitschix@users.noreply.github.com>
2025-07-21 21:31:39 +0200

    save py3status log to xdg state home ; set xdg state variable

--- config/zsh/custom/xdgbase-conf.zsh
+++ config/zsh/custom/xdgbase-conf.zsh
@@ -1,5 +1,6 @@
 # setup XDG paths
 export XDG_DATA_HOME=${XDG_DATA_HOME:="$HOME/.local/share"}
+export XDG_STATE_HOME=${XDG_STATE_HOME:="$HOME/.local/state"}
 export XDG_CACHE_HOME=${XDG_CACHE_HOME:="$HOME/.cache"}
 export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:="$HOME/.config"}
 
