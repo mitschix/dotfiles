e414dd4c121d06bb9e03d1c27b91869464aa7b37
mitschix <23629789+mitschix@users.noreply.github.com>
2026-06-30 22:09:54 +0200

    feat: set git dir and unify the way var + strings are set

--- config/zsh/custom/xdgbase-conf.zsh
+++ config/zsh/custom/xdgbase-conf.zsh
@@ -7,23 +7,25 @@ export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:="$HOME/.config"}
 # disable less history
 export LESSHISTFILE=-
 
+# add gitdir
+export GIT_DIR="$XDG_CONFIG_HOME/git"
 # move go home
 export GOPATH="$XDG_DATA_HOME/go"
 # move ruby .cargo dir
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
 # move .npm/npm -> ! npmrc need to be created and edited
-export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME"/npm/npmrc
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
 # move .xinitrc -> ! need to be placed at startx >exec startx "$XDG_CONFIG_HOME/X11/xinitrc"
-export XINITRC="$XDG_CONFIG_HOME"/X11/xinitrc
+export XINITRC="$XDG_CONFIG_HOME/X11/xinitrc"
 # move .vscode-oss -> ! is not documented and might break unexpectedly
-export VSCODE_PORTABLE="$XDG_DATA_HOME"/vscode
+export VSCODE_PORTABLE="$XDG_DATA_HOME/vscode"
 # move .pylint.d has a won`t fix -> but a note with this
-export PYLINTHOME="$XDG_CACHE_HOME"/pylint
+export PYLINTHOME="$XDG_CACHE_HOME/pylint"
 # move .Xauthoriy -> ! breaks lightDM
-export XAUTHORITY="$XDG_RUNTIME_DIR"/Xauthority
+export XAUTHORITY="$XDG_RUNTIME_DIR/Xauthority"
 
 # move .yarnrc to new loaction
 alias yarn="yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config"
 
 # move fasder cache file
-export _FASDER_DATA="$XDG_CACHE_HOME"/fasder-cache
+export _FASDER_DATA="$XDG_CACHE_HOME/fasder-cache"
